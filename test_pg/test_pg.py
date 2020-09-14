3c57e18d09640472bdd32c8108b92d544d5aeff5
abhi1kumar <abhinav3663@gmail.com>
2020-09-14 19:23:14 -0400

    test_pg added

---
+++ test_pg/test_pg.py
@@ -0,0 +1,11 @@
+
+import os, sys
+sys.path.append(os.getcwd())
+from pg import *
+
+g = petersen_graph()
+g.draw_matplotlib(layout= "circular", path= "fig_1.png")
+
+g =  Graph({0: {1: 1, 3: 1}, 1: {0: 1}, 2: {3: 1}, 3: {0: 1, 2: 1}})
+g.draw_matplotlib(layout= "circular", path= "fig_2.png")
+g.draw_tkz_graph()
